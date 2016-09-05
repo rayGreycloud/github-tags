a2b759e551aec3780da23335303cc59f86ac2bd8
rayGreycloud <rayGreycloud@gmail.com>
2016-09-04 21:17:42 -0700

    Add router and public & repos pages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import styles from './styles/main.css'
-const Hello = React.createClass({
-  render () {
-    return <div>Hello, {this.props.name}</div>
-  }
-})
+import Router from './router'
+import styles from './styles/main.styl'
 
-React.render(<Hello name="ray"/>, document.body)
+window.app = {
+  init () {
+    this.router = new Router()
+    this.router.history.start()
+  }
+}
+
+window.app.init()
